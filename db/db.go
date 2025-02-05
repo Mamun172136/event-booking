ef330e8d47f61a6473ec104ba8d4693997454e07
Mamun172136 <ma1536204@gmail.com>
2025-02-05 20:20:13 +0600

    create event table

--- db/db.go
+++ db/db.go
@@ -6,6 +6,34 @@ import (
 	_"github.com/mattn/go-sqlite3"
 )
 
-func InitDB(){
-	sql.Open("sqlite3", "api.db")
+var DB *sql.DB
+ 
+func InitDB() {
+    var err error
+    DB, err = sql.Open("sqlite3", "api.db")
+ 
+    if err != nil {
+        panic("Could not connect to database.")
+    }
+ 
+    DB.SetMaxOpenConns(10)
+    DB.SetMaxIdleConns(5)
+ 
+    createTables()
+}
+
+func createTables(){
+	createEventsTable := `CREATE TABLE IF NOT EXISTS events (
+		id				INTEGER PRIMARY KEY AUTOINCREMENT
+		name			TEXT NOT NULL,
+		description		TEXT NOT NULL,
+		location		TEXT NOT NULL,
+		dateTime		DATETIME NOT NULL,
+		user_id			INTEGER
+	
+	)`
+	_,err:=DB.Exec(createEventsTable)
+	if err != nil {
+        panic("Could not connect to database.")
+    }
 }
