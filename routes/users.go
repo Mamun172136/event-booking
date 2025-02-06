d8df21095bb42eeb45a877b6d2675f4e55f2a3df
Mamun172136 <ma1536204@gmail.com>
2025-02-07 03:22:44 +0600

    added create jwt token

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/models"
+	"example.com/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -42,6 +43,12 @@ func login(context *gin.Context){
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "login successful!!"})
+	token,err :=utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil{
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not generate token."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "login successful!!", "token":token})
 }
 
