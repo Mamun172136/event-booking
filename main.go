1f5a43e6d04b154850d0e7063621bea54f5a51b1
Mamun172136 <ma1536204@gmail.com>
2025-02-06 09:44:06 +0600

    added get query for single event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/db"
 	"example.com/models"
@@ -12,6 +13,7 @@ func main (){
 	db.InitDB()
 	server:=gin.Default()
 	server.GET("/events",getEvents)
+	server.GET("/events/:id",getEvent)
 	server.POST("/events",createEvent)
 	server.Run(":5000")
 }
@@ -23,6 +25,20 @@ func getEvents(context *gin.Context){
 		return
 	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context){
+    eventId,err:= strconv.ParseInt(context.Param("id"),10, 64)	
+	if err != nil{
+		context.JSON(http.StatusBadGateway, gin.H{"message":"could no parse event id."})
+		return
+	}
+	event,err:=models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message":"could no fetch events, try again later."})
+		return
+	}
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context){
