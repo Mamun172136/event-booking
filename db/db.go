ae7b04f3320da64f4b4af9f96d73896f6e1cec44
Mamun172136 <ma1536204@gmail.com>
2025-04-27 21:37:06 +0600

    update route

--- db/db.go
+++ db/db.go
@@ -49,4 +49,23 @@ func createTables(){
 	if err != nil {
         panic("Could not create events table")
     }
+
+	createdRegistrationsTable := `
+		CREATE TABLE IF NOT EXISTS registrations (
+		
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id   INTEGER
+
+		FOREIGN KEY(event_id) REFERENCES events(id)
+		FOREIGN KEY(user_id  ) REFERENCES users(id)		)
+	
+	`
+
+	_,err= DB.Exec(createdRegistrationsTable)
+
+	if err != nil {
+        panic("Could not create registration table")
+    }
 }
+
---
+++ routes/register.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context){
+
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil{
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse event id."})
+		return
+	}
+
+	event,err:=models.GetEventById(eventId)
+
+	if err != nil{
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
+	}
+
+	err = event.Register(userId)
+
+	if err != nil{
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not register for  event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": " registered for  event"})
+
+}
